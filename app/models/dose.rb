aae958cdc18a173b5ba8e1774a6ef7721926adab
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-01 14:59:56 -0300

    Heroku

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,9 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-
+  validates :cocktail, uniqueness: { scope: :ingredient }
   validates :description, presence: true
-  validates :cocktail, presence: true
-  validates :ingredient, presence: true
-  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingred_serialized = open(url).read
+ingred = JSON.parse(ingred_serialized)
+
+ingred['drinks'].each do |drink|
+  ingredient = Ingredient.create(name: drink['strIngredient1'])
+  puts ingredient.name
+end
+
+
